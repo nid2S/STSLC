92e7f40e9402902ff2aeda0c921a86c929a90b95
S20034N\user <s20034@gsm.hs.kr>
2021-07-20 17:59:57 +0900

    finish ksl dataset

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,12 +1,14 @@
 import selenium.webdriver
 from urllib.request import urlretrieve
+from urllib.error import URLError
 from selenium.common.exceptions import ElementNotInteractableException, UnexpectedAlertPresentException
 # dataset resource : https://sldict.korean.go.kr/front/sign/signList.do?top_category=CTE
+# word_len : 3847+1 (except duplicate : 3818)
 
 def get_ksl_data():
     driver = selenium.webdriver.Chrome("D:\\python_util\\driver\\chromedriver.exe")
     driver.get('https://sldict.korean.go.kr/front/sign/signContentsView.do?current_pos_index=0&origin_no=10127&searchWay=&top_category=CTE&category=&detailCategory=&searchKeyword=&pageIndex=1&pageJumpIndex=')
-    f = open("./dataset/ksl_data/words.txt", "w+", encoding="UTF-8")
+    f = open("./dataset/ksl_data/words.txt", "a+", encoding="UTF-8")  # for unexpected finished, file type is a (have to change count)
     count = 0
     while True:
         try:
@@ -17,17 +19,27 @@ def get_ksl_data():
             korean_element = driver.find_element_by_xpath('//div[@class="main_contents"]/form[@id="signViewForm"]/dl[@class="content_view_dis"]/dd')
             word = korean_element.text
             # save video, korean
-            f.write(str(count)+"\t"+word+"\n")
-            urlretrieve(video_src, "./dataset/ksl_data/"+str(count)+".mp4")
+            try:
+                urlretrieve(video_src, "./dataset/ksl_data/"+str(count)+".mp4")
+                f.write(str(count)+"\t"+word+"\n")
+            except URLError:
+                driver.refresh()
+                continue
             count += 1
             # if end the list, break (or direct stop)
-            try:
-                driver.find_element_by_xpath('//div[@class="btn_set mt_30"]/a[@path="/images/tooltip/next.gif"]').click()
-            except ElementNotInteractableException:
-                break
+            driver.find_element_by_xpath('//div[@class="btn_set mt_30"]/a[@path="/images/tooltip/next.gif"]').click()
+
         except UnexpectedAlertPresentException:  # if error, refresh
+            if count > 3850:  # word number + a(for error).
+                break
             driver.refresh()
             continue
 
     driver.close()
     f.close()
+
+
+def eng_preprocessing(sent):
+    pass
+
+
