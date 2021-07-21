be6a400c02939981e8d182424ac6207ad4867dc4
S20034N\user <s20034@gsm.hs.kr>
2021-07-21 23:05:31 +0900

    isl dataset | preprocessing plan

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,13 +1,14 @@
 import re
+import time
 import pandas as pd
 import selenium.webdriver
 from urllib.request import urlretrieve
 from urllib.error import URLError
-from selenium.common.exceptions import ElementNotInteractableException, UnexpectedAlertPresentException
-# dataset resource : https://sldict.korean.go.kr/front/sign/signList.do?top_category=CTE
-# word_len : 3847+1 (except duplicate : 3818)
+from selenium.common.exceptions import UnexpectedAlertPresentException, NoSuchElementException
 
 def get_ksl_data():
+    # dataset resource : https://sldict.korean.go.kr/front/sign/signList.do?top_category=CTE
+    # word_len : 3847+1 (except duplicate : 3818)
     driver = selenium.webdriver.Chrome("D:\\python_util\\driver\\chromedriver.exe")
     driver.get('https://sldict.korean.go.kr/front/sign/signContentsView.do?current_pos_index=0&origin_no=10127&searchWay=&top_category=CTE&category=&detailCategory=&searchKeyword=&pageIndex=1&pageJumpIndex=')
     f = open("./dataset/ksl_data/words.txt", "a+", encoding="UTF-8")  # for unexpected finished, file type is a (have to change count)
@@ -41,6 +42,38 @@ def get_ksl_data():
     f.close()
 
 
+def get_isl_data():
+    # dataset resource : http://www.sematos.eu/isl.html
+    driver = selenium.webdriver.Chrome("D:\\python_util\\driver\\chromedriver.exe")
+    driver.get("http://www.sematos.eu/isl.html")
+    f = open("./dataset/isl_data/word.txt", "w+")
+    count = 0
+    while True:
+        for i in range(22):
+            time.sleep(5)  # if not loaded, raise IndexError.
+            driver.find_elements_by_xpath('//div[@id="dico_mots"]/div/ul/li')[i]\
+                .find_element_by_xpath('./a[@class="tooltip"]').click()
+            time.sleep(2)
+            try:
+                src = driver.find_element_by_xpath('//div[@id="centre"]/div[@id="playermot"]/div/video/source[@type="video/mp4"]').get_attribute("src")
+            except NoSuchElementException:
+                time.sleep(2)
+                src = driver.find_element_by_xpath('//div[@id="centre"]/div[@id="playermot"]/div/video/source[@type="video/mp4"]').get_attribute("src")
+            word = re.sub(r"\(\w+\)", "", driver.find_element_by_xpath('//div[@id="centre"]/h2[@id="titremot"]').text).strip()
+
+            urlretrieve(src, "./dataset/isl_data/"+str(count)+".mp4")
+            f.write(str(count)+"\t"+word+"\n")
+            count += 1
+
+            driver.back()
+
+        try:
+            driver.find_element_by_xpath('//div[@id="dico_mots"]/div/img[@class="rightt"]').click()
+        except NoSuchElementException:
+            pass  # have to stop manually
+
+get_isl_data()
+
 def eng_preprocessing(sent: str):
     # lowcase, remove Abbreviated & non-english(+|'|,| |)char
     sent = re.sub(r"[^a-z0-9' ]", "", sent.lower())
@@ -53,3 +86,12 @@ def eng_preprocessing(sent: str):
     # split word, divide char level
     result_sent = [[char for char in word] for word in sent_sr.values]
     return result_sent
+
+
+def kor_preprocessing(sent: str):
+    pass
+
+
+def eng_isl_preprocessing(sent: str):
+    pass
+
