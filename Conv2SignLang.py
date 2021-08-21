c0bd914c3ac5ac074a79d7eaee127e4847a63804
S20034N\user <s20034@gsm.hs.kr>
2021-08-21 22:52:40 +0900

    show with plt

--- Conv2SignLang.py
+++ Conv2SignLang.py
@@ -2,11 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def conv_eng(sents: list[list[list[str]]]):
+    # sent > word > char
+    plt.style.use('seaborn-white')
     for sent in sents:
-        for word in sent:
-            for char in word:
+        for i, word in enumerate(sent):
+            for j, char in enumerate(word):
+                # 플롯간 간격 조절
+                idx = i*max(len(w) for w in sent) + (j+1)
+                plt.subplot(len(sent), max(len(w) for w in sent), idx)  # (row, col) = (num of words, max length of word). make new screen for each sent.
+                plt.title(char)
+                plt.xticks([])
+                plt.yticks([])
                 plt.imshow(plt.imread("./dataset/asl_data/"+char+".png"))
-                plt.show()
+        plt.show()
+
 
 def conv_kor(sents: list[list[str]]):
     pass
