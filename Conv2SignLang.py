635e0366a07b224e434a626cdfe6ecc0739ad4b2
S20034N\user <s20034@gsm.hs.kr>
2021-08-22 12:02:00 +0900

    show with plt(gap)

--- Conv2SignLang.py
+++ Conv2SignLang.py
@@ -7,9 +7,9 @@ def conv_eng(sents: list[list[list[str]]]):
     for sent in sents:
         for i, word in enumerate(sent):
             for j, char in enumerate(word):
-                # 플롯간 간격 조절
                 idx = i*max(len(w) for w in sent) + (j+1)
                 plt.subplot(len(sent), max(len(w) for w in sent), idx)  # (row, col) = (num of words, max length of word). make new screen for each sent.
+                plt.subplots_adjust(hspace=0.45)  # 플롯간 간격 조절
                 plt.title(char)
                 plt.xticks([])
                 plt.yticks([])
