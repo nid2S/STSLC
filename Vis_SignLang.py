e09d4745448737805b3539753887ae615897d657
nid2S <s20034@gsm.hs.kr>
2021-09-01 23:41:45 +0900

    vis_kor | plan

--- Vis_SignLang.py
+++ Vis_SignLang.py
@@ -1,5 +1,9 @@
 import tkinter
 import pandas as pd
+from hgtk.text import decompose
+from cv2 import VideoCapture
+from PIL.Image import fromarray
+from PIL.ImageTk import PhotoImage
 
 
 def vis_eng(sents: list[list[list[str]]]):
@@ -44,7 +48,14 @@ def vis_eng(sents: list[list[list[str]]]):
 def vis_kor(sents: list[list[str]]):
     # sent > token
 
-    data = pd.read_csv("./STSLC/dataset/ksl_data/words.txt", header=None,  names=["file_num", "word"], sep="\t")
+    # 토큰이 있는 열의 파일넘버(or 인덱스)를 가져옴(없으면 쪼개서)> 해당 파일넘버로 비디오를 가져와 비디오 출력 > 그대로 쭉 끝까지 > 끝나면 디스트로이 되게
+    # 파일넘버를 가져오는 방법 | 1. data에 ,가 없게 가공 후 loc조건문(열에 파일넘버 추가)
+    # 파일넘버를 가져오는 방법 | 2. 반복문 돌려서 인덱스 찾고 없으면 나눠서 > 2-1.처음부터 다시 2-2.자/모음만 모아다가
+    data = pd.read_csv("./STSLC/dataset/ksl_data/words.txt", header=None,  index_col=0, names=["word"], sep="\t")
+    for sent in sents:
+        for token in sent:
+
+
 
 
 def vis_eng_isl(sents: list[list[str]]):
