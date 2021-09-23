6dc4d08daac235711d04064cc9d6154e3fd263a5
nid2S <s20034@gsm.hs.kr>
2021-09-23 23:23:45 +0900

    fix some, functional

--- tokenizer/tokenization_vocab.py
+++ tokenizer/tokenization_vocab.py
@@ -2,30 +2,38 @@ import MeCab
 import pandas as pd
 import re
 
-def tokenize(text):
-    # vocab text 전처리
-    text = re.sub("(-었)|(편지 등을)|(꽃이)|(해가)|(鬼神)", "", text)
-    text = re.sub("\W", "", text)
+def get_ksl():
+    def tokenize(text):
+        # vocab text 전처리
+        text = re.sub("(-었)|(편지 등을)|(꽃이)|(해가)|(鬼神)", "", text)
+        text = re.sub("\W", "", text)
 
-    # 문장 토큰화
-    t = MeCab.Tagger()
-    result_list = []
-    for line in t.parse(text).split("\n"):
-        result_list.append(line.split("\n")[0])
-    return result_list
+        # 문장 토큰화
+        t = MeCab.Tagger()
+        result_list = []
+        for line in t.parse(text).split("\n"):
+            w = line.split("\t")[0]
+            if w in ["", "EOS"]:
+                continue
+            result_list.append(w)
+        return result_list
+
+    # make csv_file
+    ksl = pd.read_csv("D:\\workspace\\Git_project\\STSLC\\dataset\\ksl_data\\words.txt", sep="\t", names=["file_num", "word"])
+    file_num = []
+    word_origin = []
+    word_encoded = []
+    # put each word in list
+    for i, row in ksl.iterrows():
+        for word in row["word"].split(","):
+            file_num.append(row["file_num"])
+            word_origin.append(word)
+            word_encoded.append(tokenize(word))
+    # save to csv with DataFrame
+    df = pd.DataFrame()
+    df["file_num"] = pd.Series(file_num)
+    df["word_origin"] = pd.Series(word_origin)
+    df["word_encoded"] = pd.Series(word_encoded)
+    df.to_csv("D:\\workspace\\Git_project\\STSLC\\dataset\\ksl_encoded_data.csv", index=False, header=False, columns=["file_num", "word_origin", "word_encoded"])
 
 
-ksl = pd.read_csv("D:\\workspace\\Git_project\\STSLC\\dataset\\ksl_data\\words.txt", sep="\t", names=["file_num", "word"])
-file_num = []
-word_origin = []
-word_encoded = []
-for i, row in ksl.iterrows():
-    for word in row["word"].split(","):
-        file_num.append(row["file_num"])
-        word_origin.append(word)
-        word_encoded.append(tokenize(word))
-df = pd.DataFrame()
-df["file_num"] = pd.Series(file_num)
-df["word_origin"] = pd.Series(word_origin)
-df["word_encoded"] = pd.Series(word_encoded)
-df.to_csv("D:\\workspace\\Git_project\\STSLC\\dataset\\ksl_encoded_data.csv", index=False, header=False)  # header=[열이름], columns=[열이름]
